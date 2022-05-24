8cfed7b8c939d3d88f3b1873741eeb1fbdaac10b
KomarovaZlata <komarovazlata.2008@mail.ru>
2022-05-24 19:53:25 +0300

    Мой первый опыт

--- pr.py
+++ pr.py
@@ -1,3 +1,14 @@
-print("Привет, как тебя зовут?")
-s = input()
-print(s+', Очень рада тебя видеть')
+import random
+#print("Привет, как тебя зовут?")
+#s = input()
+#print(s+', Очень рада тебя видеть')
+#st = 'сыр '*5
+#print(st)
+#ch = (2+10)/2*3+2
+#print(ch)
+s = random.randint(1,10)
+print('сгенерировано число: '+str(s))
+for i in range(5):
+    x = random.randint(1,10)
+    s = s + x
+    print('s+x='+str(s))
